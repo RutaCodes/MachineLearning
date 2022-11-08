231fa54f67fa7fd33c8518264db7bd9928a6e478
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-11-08 15:53:15 -0500

    Update run_models_x.py

--- StratifiedShuffleSplit/run_models_x.py
+++ StratifiedShuffleSplit/run_models_x.py
@@ -7,13 +7,11 @@ Created on Tue Nov  8 14:26:35 2022
 """
 
 import pandas as pd
-#import numpy as np
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor,GradientBoostingRegressor
 from sklearn.model_selection import cross_validate
-#from sklearn.model_selection import StratifiedKfold 
 
 def run_models_x(x_train: pd.DataFrame , y_train: pd.DataFrame):
     
@@ -29,15 +27,10 @@ def run_models_x(x_train: pd.DataFrame , y_train: pd.DataFrame):
     
     for name, model in models:
         
-        #x_train_no_nr = np.delete(x_train,0,1)
-        #cv_custom = StratifiedKfold(n_split=4,shuffle=True)
-        #scores = cross_validate(model, x_train_no_nr, y_train, groups=x_train[:,0], cv=cv_custom, scoring='r2',return_train_score=True)
         scores = cross_validate(model, x_train, y_train, cv=5, scoring='r2',return_train_score=True)
         Test_r2 = scores['test_score']
         Train_r2 = scores['train_score']
         
-        #MSE = cross_validation.cross_val_score(model, x_train, y_train, cv=5, scoring='mean_squared_error').mean()
-            
         df = df.append({'Model': name, 'Aver. Test R2': Test_r2.mean(), 'Aver. Train R2': Train_r2.mean()}, ignore_index = True)
         
     return df.sort_values(by=['Aver. Test R2'], ascending=False)
