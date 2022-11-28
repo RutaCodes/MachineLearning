125b45e19831caa23b9023f6d7db1cec8f9f000a
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-11-28 14:21:13 -0500

    Update Sample_size_accuracy.py

--- Sample_Nr_vs_Model_Performance/Sample_size_accuracy.py
+++ Sample_Nr_vs_Model_Performance/Sample_size_accuracy.py
@@ -51,7 +51,7 @@ for i in Ratio:
 #%%    
 #Convert % of samples to sample number 
 Samp_nrs_split = (Ratio * len(Atrazine_conc_ar)).astype(int) #for test
-Samp_nrs_split_train = ((1-Ratio) * len(Atrazine_conc_ar)).astype(int)  #for test
+Samp_nrs_split_train = ((1-Ratio) * len(Atrazine_conc_ar)).astype(int)  #for training
 
 # Flipping data frame, son that it would start with the lowest number of training samples
 df_flip = df_train[::-1] 
@@ -68,7 +68,7 @@ plt.ylabel("R2")
 plt.title("How models results depend on number of training samples?")
 plt.legend()
 
-#%%
+# Test results
 plt.plot(Samp_nrs_split, df_test["LinReg"].astype(float), label="LinReg") 
 plt.plot(Samp_nrs_split, df_test["SVR"].astype(float), label="SVR") 
 plt.plot(Samp_nrs_split, df_test["RF"].astype(float), label="RF") 
@@ -77,43 +77,4 @@ plt.plot(Samp_nrs_split, df_test["XGB"].astype(float), label="XGBoost")
 plt.xlabel("Number of test samples")
 plt.ylabel("R2")
 plt.title("How models results depend on number of test samples?")
-plt.legend()
-#%% See how this changes if dataset without outliers were used
-
-# LOAD DATASET WITHOUT OUTLIERS
-
-#Sample data, statial and temporal variables - trasformed (normalizing data distribution)
-All_tr_variables_no_out_DF = pd.read_csv('All_variables_wJday_without_outliers.csv')
-All_tr_variables_no_out_arr = np.array(All_tr_variables_no_out_DF)
-
-#Import non-transformed concentration values
-Atrazine_conc_no_out_DF = pd.read_csv('Atrazine_conc_org_without_outliers.csv')
-Atrazine_conc_no_out_ar = np.array(Atrazine_conc_no_out_DF) 
-
-#Predictor and dependant variables
-Expl_var_wJ_no_out = np.delete(All_tr_variables_no_out_arr,[0, 1, 2],1).astype(float)
-Depend_var_no_out = All_tr_variables_no_out_arr[:, 2].reshape(-1,1)
-
-#%%
-df_no_out_train = pd.DataFrame()
-df_no_out_test = pd.DataFrame()
-for i in Ratio:
-    x_train_rn_no_out, x_test_rn_no_out, y_train_rn_no_out, y_test_rn_no_out = train_test_split(
-    Expl_var_wJ_no_out, Depend_var_no_out, test_size=i, random_state=42)
-    Results_no_out = run_models_sample_test(x_train_rn_no_out, y_train_rn_no_out,score)
-    Res_df_line_no_out_train = pd.concat([pd.DataFrame([i], columns=["Sample_size"]), Results_no_out[0]], axis=1)
-    Res_df_line_no_out_test = pd.concat([pd.DataFrame([i], columns=["Sample_size"]), Results_no_out[1]], axis=1)
-    df_no_out_train = df_no_out_train.append(Res_df_line_no_out_train)
-    df_no_out_test = df_no_out_test.append(Res_df_line_no_out_test)
-    
-    
-df_no_out_train_flip = df_no_out_train[::-1] 
-plt.plot(Samp_nrs_split_train, df_no_out_train_flip["LinReg"].astype(float), label="LinReg") 
-plt.plot(Samp_nrs_split_train, df_no_out_train_flip["SVR"].astype(float), label="SVR") 
-plt.plot(Samp_nrs_split_train, df_no_out_train_flip["RF"].astype(float), label="RF") 
-plt.plot(Samp_nrs_split_train, df_no_out_train_flip["GBC"].astype(float), label="GBC")
-plt.plot(Samp_nrs_split_train, df_no_out_train_flip["XGB"].astype(float), label="XGBoost")  
-plt.xlabel("Number of training samples")
-plt.ylabel("R2")
-plt.title("How models results depend on number of training samples?")
 plt.legend()
